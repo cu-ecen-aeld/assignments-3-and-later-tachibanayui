b84902320aaeec11963d6bcdc6ef5e9284f0018a
shutano <shutano.work@gmail.com>
2025-08-05 05:25:12 +0700

    step 4c

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -66,6 +66,7 @@ set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
 	echo "success"
+	/usr/bin/writer "/tmp/assignment4-result.txt" "${OUTPUTSTRING}"
 	exit 0
 else
 	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
